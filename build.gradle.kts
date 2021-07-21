86989b0d4ae9838ab1429c4b96c45024b7a0a436
rafael.possenti <rafael41051010>
2021-07-21 18:42:17 -0300

    actuator rou for post

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,7 +19,6 @@ extra["springCloudVersion"] = "2020.0.2"
 
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-webflux")
-	implementation("org.springframework.boot:spring-boot-starter-actuator")
 
 	implementation("org.springframework.cloud:spring-cloud-starter-gateway")
 	implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-client")
